c0bb53ed9a3833c4ec353a15d94aab5429cec31a
davallejo <djdavmayumy@hotmail.com>
2024-04-03 11:12:40 -0500

    Agregando archivos del proyecto by Diego Vallejo

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+# Autor: Diego Armando Vallejo Vinueza
+
+from mean_var_std import calculate
+
+# Ejemplo de uso
+input_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+# Llama a la función calculate e imprime el resultado
+result = calculate(input_list)
+print(result)
